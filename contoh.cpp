5301d0887056bd4e9b89f9521376b1168c194516
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-05-06 19:46:12 +0700

    class persegi panjang

---
+++ contoh.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: // akses modifier
+    int panjang, lebar;
+
+public:
+    void input()
+    {
+        cout << "Masukkan Panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> lebar
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+    //Lingkaran
+};
