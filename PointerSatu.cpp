f50a945791cf26a1c0693ba42d483d91b38fbf0e
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-05-06 20:28:11 +0700

    implementasi akses objek mahasiswa melalui referensi dan pointer

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -7,3 +7,17 @@ class mahasiswa{
         cout << "No Induk = " << nim << endl;        
     }
 };    
+
+int main () {
+    mahasiswa mhs{1}; // Objek MHS
+    mhs.showNim(); //Member Acces Operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs ->nim = 3;
+    pMhs ->showNim();
+    return 0;
+};
